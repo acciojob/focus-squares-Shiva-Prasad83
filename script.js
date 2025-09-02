93136674107bd0e1aae46ab7f1ff48cd11272b74
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-02 22:26:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
 //your JS code here. If required.
-const div1=document.getElementById("square1");
-const div2=document.getElementById("square2");
-const div3=document.getElementById("square3");
-
-div1.addEventListener("mouseover",function(){
-	div2.style.backgroundColor="rgb(111, 78, 55)";
-	div3.style.backgroundColor="rgb(111, 78, 55)";
-});
-div1.addEventListener("mouseout",function(){
-	div2.style.backgroundColor="rgb(230, 230, 250)";
-	div3.style.backgroundColor="rgb(230, 230, 250)";
-});
-
-div2.addEventListener("mouseover",function(){
-	div1.style.backgroundColor="rgb(111, 78, 55)";
-	div3.style.backgroundColor="rgb(111, 78, 55)";
-});
-div2.addEventListener("mouseout",function(){
-	div1.style.backgroundColor="rgb(230, 230, 250)";
-	div3.style.backgroundColor="rgb(230, 230, 250)";
-});
-div3.addEventListener("mouseover",function(){
-	div2.style.backgroundColor="rgb(111, 78, 55)";
-	div1.style.backgroundColor="rgb(111, 78, 55)";
-});
-div3.addEventListener("mouseout",function(){
-	div2.style.backgroundColor="rgb(230, 230, 250)";
-	div1.style.backgroundColor="rgb(230, 230, 250)";
-});
+let squares=document.getElementsByClassName("square");
+for(let square of squares){
+	square.addEventListener("mouseover",(e)=>{
+		for(let i=0;i<squares.length;i++){
+			if(e.target.id!=squares[i].id){
+				squares[i].style.backgroundColor="#6F4E37";
+			}
+		}
+	});
+	square.addEventListener("mouseout",(e)=>{
+		for(let i=0;i<squares.length;i++){
+			if(e.target.id!=squares[i].id){
+				squares[i].style.backgroundColor="#E6E6FA";
+			}
+		}
+	})
+}
