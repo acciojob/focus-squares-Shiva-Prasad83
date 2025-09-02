314a8d55bb0f67bdd2f13c64a3004518f67f8b59
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-02 16:10:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,28 +3,28 @@ const div1=document.getElementById("square1");
 const div2=document.getElementById("square2");
 const div3=document.getElementById("square3");
 
-div1.addEventListener("mouseenter",function(){
+div1.addEventListener("mouseover",function(){
 	div2.style.backgroundColor="rgb(111, 78, 55)";
 	div3.style.backgroundColor="rgb(111, 78, 55)";
 });
-div1.addEventListener("mouseleave",function(){
+div1.addEventListener("mouseout",function(){
 	div2.style.backgroundColor="rgb(230, 230, 250)";
 	div3.style.backgroundColor="rgb(230, 230, 250)";
 });
 
-div2.addEventListener("mouseenter",function(){
+div2.addEventListener("mouseover",function(){
 	div1.style.backgroundColor="rgb(111, 78, 55)";
 	div3.style.backgroundColor="rgb(111, 78, 55)";
 });
-div2.addEventListener("mouseleave",function(){
+div2.addEventListener("mouseout",function(){
 	div1.style.backgroundColor="rgb(230, 230, 250)";
 	div3.style.backgroundColor="rgb(230, 230, 250)";
 });
-div3.addEventListener("mouseenter",function(){
+div3.addEventListener("mouseover",function(){
 	div2.style.backgroundColor="rgb(111, 78, 55)";
 	div1.style.backgroundColor="rgb(111, 78, 55)";
 });
-div3.addEventListener("mouseleave",function(){
+div3.addEventListener("mouseout",function(){
 	div2.style.backgroundColor="rgb(230, 230, 250)";
 	div1.style.backgroundColor="rgb(230, 230, 250)";
 });
